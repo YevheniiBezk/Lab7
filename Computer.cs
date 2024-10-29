f8e4a8fb5786d6d2a70a73a5d239552ffcf043a1
Женя Безкуцький <bezkuczkij@gmail.com>
2024-10-30 00:44:14 +0200

    Добавьте файлы проекта.

---
+++ Computer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Lab7
+{
+    public class Computer : IComputer, IComparable<Computer>
+    {
+        public string ModelName { get; set; }
+        public string Processor { get; set; }
+        public int RAM { get; set; }
+        public int Storage { get; set; }
+        public double Price { get; set; }
+        public bool HasGraphicsCard { get; set; }
+        public bool HasWiFi { get; set; }
+
+        public Computer() { }
+
+        public Computer(string modelName, string processor, int ram, int storage, double price, bool hasGraphicsCard, bool hasWiFi)
+        {
+            ModelName = modelName;
+            Processor = processor;
+            RAM = ram;
+            Storage = storage;
+            Price = price;
+            HasGraphicsCard = hasGraphicsCard;
+            HasWiFi = hasWiFi;
+        }
+
+        public int CompareTo(Computer other)
+        {
+            if (other == null) return 1;
+            return ModelName.CompareTo(other.ModelName);
+        }
+
+        public override string ToString()
+        {
+            return $"{ModelName}, Процесор: {Processor}, ОЗП: {RAM} ГБ, Пам'ять: {Storage} ГБ, Ціна: {Price:C}, Відеокарта: {(HasGraphicsCard ? "так" : "ні")}, WiFi: {(HasWiFi ? "так" : "ні")}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "computer.bin");
+
+            List<Computer> computers = ReadComputers(filePath);
+
+            computers.Sort();
+
+            Console.WriteLine("\nВідсортований список комп'ютерів:");
+            foreach (var comp in computers)
+            {
+                Console.WriteLine(comp.ToString());
+            }
+
+            Console.WriteLine("\nНатисніть Enter, щоб завершити...");
+            Console.ReadLine();
+        }
+
+        static List<Computer> ReadComputers(string filePath)
+        {
+            List<Computer> computers = new List<Computer>();
+
+            try
+            {
+                using (BinaryReader br = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    while (br.BaseStream.Position != br.BaseStream.Length)
+                    {
+                        string modelName = br.ReadString();
+                        string processor = br.ReadString();
+                        int ram = br.ReadInt32();
+                        int storage = br.ReadInt32();
+                        double price = br.ReadDouble();
+                        bool hasGraphicsCard = br.ReadBoolean();
+                        bool hasWiFi = br.ReadBoolean();
+
+                        Computer computer = new Computer(modelName, processor, ram, storage, price, hasGraphicsCard, hasWiFi);
+
+                        computers.Add(computer);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка при читанні файлу: {ex.Message}");
+            }
+
+            return computers;
+        }
+    }
+}
